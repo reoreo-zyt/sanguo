29e5a93c818378076d795d13f48132eb91d002cf
reoreo-zyt <768119359@qq.com>
2024-08-31 04:12:57 +0800

    feat: 参考代码格式

--- scripts/RadarChartStats.gd
+++ scripts/RadarChartStats.gd
@@ -9,11 +9,6 @@ extends Control
 @export var stats = [80,80,80,80,80,80,80,80]
 @export var labelPos = 1.3
 
-#var lineCount = 8
-#var labels = ["SHO","SPE","POW","DEF","GK","BOD","MID","ATK"]
-#var stats = [50,50,50,50,50,50,50,50]
-#var labelPos = 1.3
-
 func update_radar_stats():
 	base.update()
 	main.update()
