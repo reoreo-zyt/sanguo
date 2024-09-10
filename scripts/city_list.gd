e5a86135cfd8d53c9fa3451ed896926fbf8d2c85
reoreo-zyt <768119359@qq.com>
2024-09-10 23:47:49 +0800

    feat: 军事

--- scripts/city_list.gd
+++ scripts/city_list.gd
@@ -1,7 +1,11 @@
 extends Control
 
+var arrtValue = preload("res://scenes/attr_value.tscn")
+var character_instances = []
+
 func _ready() -> void:
 	get_city_list()
-	
+
 func get_city_list():
+	# 获取所有城市
 	pass
