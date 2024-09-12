fb28ecc68b3ce279d141e90e9047ccce38b8ce36
reoreo-zyt <768119359@qq.com>
2024-09-12 23:04:21 +0800

    feat: 选择城市

--- scripts/city_list.gd
+++ scripts/city_list.gd
@@ -4,15 +4,15 @@ var cityValue = preload("res://scenes/city_value.tscn")
 var city_instances = []
 
 func _ready() -> void:
-	SignalBus.connect("change_battle_city", _on_change_battle_city)
+	SignalBus.connect("select_city_for_fight_begin", _on_select_city_for_fight_begin)
 
-func get_city_list():
+func get_city_list(city_id):
 	if(city_instances.size()):
 		for city_insatnce in city_instances:
 			city_insatnce.queue_free()
 	city_instances = []
 	# 获取主公在的那个城市，计算其他城市到这个城市的距离
-	var cur_city = Global.characters[Global.cur_character].cityId
+	var cur_city = city_id
 	for city in Global.citys:
 		var city_insatnce = cityValue.instantiate()
 		city_insatnce.city_name = Global.set_name_text(Global.citys[city].name)
@@ -23,12 +23,12 @@ func get_city_list():
 		if(calc_get_day(calc_distance(Vector2(Global.citys[city].position_x, Global.citys[city].position_y), Vector2(Global.citys[cur_city].position_x, Global.citys[cur_city].position_y))) <= 10):
 			$ScrollContainer/MarginContainer/VBoxContainer.add_child(city_insatnce)
 
-func _on_change_battle_city():
-	get_city_list()
-
 func calc_distance(location1, location2):
 	# 武威到天水距离是 572 公里
 	return location1.distance_to(location2) * 6.77278
 
 func calc_get_day(value):
 	return ceil(value / 80)
+
+func _on_select_city_for_fight_begin(city_id):
+	get_city_list(city_id)
